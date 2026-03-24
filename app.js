126183af176e4cea28cf6b387d2b85414b5e0f39
Diksha <cdiksha931@gmail.com>
2026-03-24 14:34:58 +0530

    Task Manager Website

--- app.js
+++ app.js
@@ -1,51 +1,38 @@
 const path = require("path");
 require('node:dns').setServers(['8.8.8.8', '1.1.1.1']); 
+
 require("dotenv").config({ path: path.join(__dirname, ".env") });
 
 const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
-const MongoStore = require('connect-mongo'); // Make sure this is v4+
 const ejsMate = require('ejs-mate');
+
+
+
 
 const authRoutes = require('./routes/auth');
 const taskRoutes = require('./routes/tasks');
 
 const app = express();
 
-// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log("MongoDB Connection Error:", err));
-
-// EJS setup
-app.engine('ejs', ejsMate);
+  .then(()=>console.log("MongoDB Connected"))
+  .catch(err=>console.log(err));
+app.engine('ejs', ejsMate); // use ejs-mate as engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Middlewares
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'public')));
 
-// Session store with MongoDB
 app.use(session({
   secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false,
-  store: MongoStore.create({ // Correct way for v4+
-      mongoUrl: process.env.MONGO_URI,
-      collectionName: 'sessions',
-      ttl: 14 * 24 * 60 * 60 // 14 days
-  }),
-  cookie: {
-      maxAge: 1000 * 60 * 60 * 24 * 14 // 14 days
-  }
+  resave:false,
+  saveUninitialized:false
 }));
 
-// Routes
 app.use('/', authRoutes);
 app.use('/tasks', taskRoutes);
 
-// Start server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(3000, ()=>console.log("Server running on http://localhost:3000"));
