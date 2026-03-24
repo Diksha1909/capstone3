5f43882334f41b7c2a9d9d3ce93597586742c931
Diksha <cdiksha931@gmail.com>
2026-03-24 15:04:05 +0530

    Task Manager Website

--- app.js
+++ app.js
@@ -34,5 +34,5 @@ app.use(session({
 
 app.use('/', authRoutes);
 app.use('/tasks', taskRoutes);
-
-app.listen(3000, ()=>console.log("listening port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=>console.log(`Server running on port ${PORT}`));
